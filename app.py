b0f462a6f6b867c925638aaf6e8a596b46acfd7d
nguyenle-clound <nguyenle19102003@gmail.com>
2024-11-19 11:18:52 +0000

    Created email spam classifier

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+cv = pickle.load(open("models/cv.pkl", 'rb'))
+clf = pickle.load(open("models/clf.pkl", 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    if request.method == 'POST':
+        email = request.form.get('content')
+    tokenized_email = cv.transform([email]) # X
+    prediction = clf.predict(tokenized_email)
+    # If the email is spam the prediction should be 1
+    prediction = 1 if prediction ==1 else -1
+    return render_template('index.html', prediction=prediction, email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
